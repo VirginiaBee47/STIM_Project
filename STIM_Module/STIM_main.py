089961cdd92db906335fc820e4273617fde444f0
David Hutchins <davidhutchins@DESKTOP-3VG750A.localdomain>
2022-06-28 11:10:54 +0900

    Added folder

---
+++ STIM_Module/STIM_main.py
@@ -0,0 +1,7 @@
+
+
+def main():
+	print("This is where our main loop will be")
+
+if __name__ == "__main__":
+	main()
---
+++ STIM_Module/__init__.py
@@ -0,0 +1,8 @@
+
+
+
+def main():
+	print("PlaceHolder")
+
+if __name__ == "__main__":x
+	main()
