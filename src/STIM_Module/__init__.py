7cd69e374171651391627feb03b15643cc4c3866
Jaxton Willman <jaxtonwillman@gmail.com>
2022-07-05 17:50:38 +0900

    Building testing again
    It builds but now I need to figure out how to organize my modules and call them from a main program. And how to access that main program from running the module.

--- src/STIM_Module/__init__.py
+++ src/STIM_Module/__init__.py
@@ -1,3 +1,5 @@
 def hello_world():
     print("Hello World")
-    
+
+def hello_world_gui():
+	print("Hello World")
--- src/STIM_Module/arghooktest.py
+++ src/STIM_Module/arghooktest.py
@@ -5,3 +5,8 @@ parser = argparse.ArgumentParser(description='Test the arghook module')
 
 parser.add_argument('-a', '--archive', action='store_true', help='Test the archive flag')
 parser.add_argument('-v', '--verbose', action='store_true', help='Test the verbose flag')
+parser.add_argument('-p', "--print_string", help="Prints the supplied argument.", default='A random string.')
+
+args = parser.parse_args()
+
+print(args.print_string)
