37c196dba912ea0424cd41b80d1ff50fa073b15a
ben-covert2020 <66711984+ben-covert2020@users.noreply.github.com>
2022-07-06 20:21:34 +0900

    added test_main to gitignore

--- src/STIM_Module/test_main.py
+++ src/STIM_Module/test_main.py
@@ -7,7 +7,7 @@ from data_processing import *
 def main():
     puuid = get_summoner("bEANS47")[0]
     recent_game_id = get_recent_game_ids(puuid)[0]
-    asyncio.run(get_raw_game_data(recent_game_id))
+    make_game_csv("bEANS47", puuid)
 
 
 if __name__ == '__main__':
